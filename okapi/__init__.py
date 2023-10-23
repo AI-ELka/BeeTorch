7bbc74472710c20b41bf2b668f4595362f892f0d
Abdessamad <abdessamadmymail@gmail.com>
2023-10-23 20:38:51 +0200

    optimized class with docs

--- okapi/__init__.py
+++ okapi/__init__.py
@@ -1,116 +1,207 @@
 import torch
 import numpy as np
+import torchmetrics
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
 
 
 class RegressionModel:
-    def __init__(self, dataX ,dataY,learning_rate=0.01,epochs=0,log=False,format=lambda x:torch.tensor(x).double(),device=False):
-        if device==False:
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        else:
-            self.device=device
+    def __init__(self, dataX, dataY, learning_rate=0.01, epochs=400, log=False, format=lambda x: torch.tensor(x).double(), device=None):
+        """
+        Initialize the RegressionModel.
+
+        Args:
+            dataX (torch.Tensor or numpy.ndarray): Input features.
+            dataY (torch.Tensor or numpy.ndarray): Target values.
+            learning_rate (float): Learning rate for the optimizer.
+            epochs (int): Number of training epochs.
+            log (bool): Whether to log training progress.
+            format (callable): A function to format input data.
+            device (str or torch.device): 'cpu' or 'cuda' for device selection.
+        """
+        self.device = device if device else 'cuda' if torch.cuda.is_available() else 'cpu'
         self.dataX = format(dataX).to(self.device)
-        if torch.is_tensor(dataY):
-            self.dataY = dataY
-        else:
-            self.dataY = torch.tensor(dataY)
-        self.dataY = self.dataY.to(self.device)
-        self.model = torch.nn.Linear(self.dataX.size()[1],self.dataY.size()[1],bias=True,dtype=float)
-        self.model.to(self.device)
+        self.dataY = torch.tensor(dataY).to(self.device)
+        self.train_data = TensorDataset(self.dataX, self.dataY)
+        self.model = torch.nn.Linear(self.dataX.size(1), self.dataY.size(1), bias=True).to(self.device)
         self.criterion = torch.nn.MSELoss()
-        self.learning_rate=learning_rate
-        self.optimizer = torch.optim.SGD(self.model.parameters(),learning_rate)
-        self.epochs=epochs
-        self.log=log
-        self.every=100
-        self.format=format
+        self.learning_rate = learning_rate
+        self.optimizer = torch.optim.SGD(self.model.parameters(), learning_rate)
+        self.epochs = epochs
+        self.log = log
+        self.every = 100
+        self.format = format
 
-    def set_log(self,log):
-        self.log=log
-    
-    def set_device(self,device=False):
-        if device==False:
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        else:
-            self.device=device
-            
-    def set_format(self,format):
-        self.format=format
-    
-    def set_learning_rate(self,lr):
-        self.learning_rate=lr
+    def set_log(self, log):
+        """Set whether to log training progress."""
+        self.log = log
 
-    def set_criterion(self,crit):
+    def set_device(self, device=None):
+        """
+        Set the device for computation.
+
+        Args:
+            device (str or torch.device): 'cpu' or 'cuda' for device selection.
+        """
+        self.device = device if device else 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.dataX = self.dataX.to(self.device)
+        self.dataY = self.dataY.to(self.device)
+
+    def set_format(self, format):
+        """Set the data formatting function."""
+        self.format = format
+
+    def set_learning_rate(self, lr):
+        """Set the learning rate for the optimizer."""
+        self.learning_rate = lr
+
+    def set_criterion(self, crit):
+        """Set the loss criterion."""
         self.criterion = crit
 
-    def set_training_data(self,dataX,dataY):
-        self.dataX=dataX
-        self.dataY=dataY
+    def set_training_data(self, dataX, dataY):
+        """
+        Set the training data.
 
-    def set_testing_data(self,dataX,dataY,format=True):
-        if format:
-            self.dataXTest=torch.tensor(dataX).double()
-        else:
-            self.dataXTest=format(dataX)
+        Args:
+            dataX (torch.Tensor or numpy.ndarray): Input features.
+            dataY (torch.Tensor or numpy.ndarray): Target values.
+        """
+        self.dataX = self.format(dataX).to(self.device)
+        self.dataY = torch.tensor(dataY).to(self.device)
+        self.train_data = TensorDataset(self.dataX, self.dataY)
 
-        if torch.is_tensor(dataY):
-            self.dataYTest=dataY
-        else:
-            self.dataYTest=torch.tensor(dataY)
+    def set_optimizer(self, opti, learning_rate=True):
+        """
+        Set the optimizer and optionally change the learning rate.
 
-    def set_optimizer(self,opti,learning_rate=True):
-        if(learning_rate==True):
-            learning_rate=self.learning_rate
-        self.optimizer = opti(self.model.parameters(),learning_rate)
+        Args:
+            opti (torch.optim.Optimizer): Optimizer instance.
+            learning_rate (bool): Whether to change the learning rate (default is True).
+        """
+        if learning_rate:
+            learning_rate = self.learning_rate
+        self.optimizer = opti(self.model.parameters(), learning_rate)
 
     def get_dimension(self):
+        """Get the dimensions of the model's weight."""
         return self.model.weight.size().detach().numpy()
 
-    def train(self,epochs=400):
-        y_predicted = self.model(self.dataX)
-        for epoch in range(self.epochs,self.epochs+epochs):
-            y_predicted = self.model(self.dataX)
+    def train(self, epochs=None, batch_size=32):
+        """
+        Train the model.
 
-            loss=self.criterion(y_predicted,self.dataY)
-            loss.backward()
+        Args:
+            epochs (int): Number of training epochs (default is the value set during initialization).
+            batch_size (int): Batch size for training data.
+        """
+        epochs = epochs if epochs is not None else self.epochs
+        train_loader = DataLoader(self.train_data, batch_size=batch_size, shuffle=True)
+        for epoch in range(self.epochs, self.epochs + epochs):
+            for batch_x, batch_y in train_loader:
+                batch_x = batch_x.to(self.device)
+                batch_y = batch_y.to(self.device)
+                y_predicted = self.model(batch_x)
+                loss = self.criterion(y_predicted, batch_y)
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+            if (epoch + 1) % self.every == 0 and self.log:
+                print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
+        self.epochs = self.epochs + epochs
 
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-            if (epoch+1)%self.every==0 and self.log: 
-                print(f'epoch: {epoch+1}, loss= {loss.item():.4f}')
-        self.epochs = self.epochs+epochs
+    def accuracy(self, format=False):
+        """
+        Compute the accuracy of the model on the test data.
 
-    def predict(self,newDataX,format=True):
-        if format:
-            newDataX = self.format(newDataX)
-        newDataX=newDataX.to(self.device)
-        return self.model(newDataX)
+        Args:
+            format (bool): Whether to format test data using the set format function.
 
-    
-    def accuracy_new(self,dataXTest,dataYTest,operator,format=True):
-        i=0
-        numberGood=0
-        if format:
-            X = self.format(dataXTest)
-        else:
-            X = dataXTest
+        Returns:
+            float: Accuracy of the model.
+        """
+        if not hasattr(self, 'dataXTest') or not hasattr(self, 'dataYTest'):
+            raise Exception("No default testing data specified. Use set_testing_data(dataXTest, dataYTest).")
+
+        X = self.format(self.dataXTest) if format else self.dataXTest
         X = X.to(self.device)
         predicted = self.model(X)
-        if self.device=='cuda':
+        if self.device == 'cuda':
             predicted = predicted.to("cpu")
-        for i in range(len(X)):
-            if operator(predicted[i].to().detach().numpy(),dataYTest[i]):
-                numberGood+=1
+        accuracy = torchmetrics.functional.accuracy(predicted, self.dataYTest)
+        return accuracy.item()
 
-        return numberGood/len(X)
+    def set_testing_data(self, dataX, dataY):
+        """
+        Set the testing data.
+
+        Args:
+            dataX (torch.Tensor or numpy.ndarray): Input features for testing.
+            dataY (torch.Tensor or numpy.ndarray): Target values for testing.
+        """
+        self.dataXTest = torch.tensor(dataX).to(self.device)
+        self.dataYTest = torch.tensor(dataY).to(self.device)
+
+
+    def save_model(self, model_path, optimizer_path=None, extra_info=None):
+        """
+        Save the trained model, optimizer state, and extra information to a file.
+
+        Args:
+            model_path (str): Path to the file where the model will be saved.
+            optimizer_path (str, optional): Path to the file where the optimizer state will be saved.
+            extra_info (dict, optional): Additional information to save alongside the model.
+        """
+        state = {
+            'epoch': self.epochs,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'extra_info': extra_info
+        }
+        torch.save(state, model_path)
+        if optimizer_path:
+            torch.save(self.optimizer.state_dict(), optimizer_path)
+
+    def load_model(self, model_path, optimizer_path=None):
+        """
+        Load a trained model, optimizer state, and extra information from a file.
+
+        Args:
+            model_path (str): Path to the file from which the model will be loaded.
+            optimizer_path (str, optional): Path to the file from which the optimizer state will be loaded.
+        """
+        checkpoint = torch.load(model_path)
+        self.epochs = checkpoint['epoch']
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        if optimizer_path:
+            self.optimizer.load_state_dict(torch.load(optimizer_path))
+
+        extra_info = checkpoint.get('extra_info', None)
+        if extra_info:
+            print("Loaded extra information:", extra_info)
+
+
+            
+if __name__ == "__main__":
+    # Example usage:
+    # Create a RegressionModel instance, set data, and train the model.
+    dataX_train = torch.rand((100, 1))  # Example training data
+    dataY_train = 2 * dataX_train + 1  # Example training labels
+    model = RegressionModel(dataX_train, dataY_train)
+    model.train(epochs=200)
     
-    def accuracy(self,operator,format=False):
-        try:
-            return self.accuracy_new(self.dataXTest,self.dataYTest,operator,format=format)
-        except NameError:
-            raise Exception("No default testing data specified, use model.set_testing_data(dataXTest,dataYTest)")
+    # Set testing data and compute accuracy
+    dataX_test = torch.rand((50, 1))  # Example testing data
+    dataY_test = 2 * dataX_test + 1  # Example testing labels
+    model.set_testing_data(dataX_test, dataY_test)
+    test_accuracy = model.accuracy()
+    print(f"Test accuracy: {test_accuracy}")
 
-    
+    # Save and load the model
+    model.save_model("my_model.pth")
+    loaded_model = RegressionModel(dataX_train, dataY_train)
+    loaded_model.load_model("my_model.pth")
+    loaded_model.set_testing_data(dataX_test, dataY_test)
+    test_accuracy_loaded = loaded_model.accuracy()
+    print(f"Test accuracy of loaded model: {test_accuracy_loaded}")
 
-
-
-        
